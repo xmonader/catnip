298ce2fda241f1606334776a77e3db5b72056b00
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2021-10-14 15:05:28 -0300

    [ipv4] Enhancement: Improving Coding Style

--- src/protocols/icmpv4/peer.rs
+++ src/protocols/icmpv4/peer.rs
@@ -122,11 +122,15 @@ impl<RT: Runtime> Icmpv4Peer<RT> {
                     "ARP query complete ({} -> {})",
                     dst_ipv4_addr, dst_link_addr
                 );
+                debug!(
+                    "reply ping ({}, {}, {})",
+                    dst_ipv4_addr, id, seq_num
+                );
                 // Send reply message.
                 rt.transmit(Icmpv4Message::new(
-                    Ethernet2Header::new(dst_link_addr, rt.local_link_addr(), EtherType2::Ipv4),
-                    Ipv4Header::new(rt.local_ipv4_addr(), dst_ipv4_addr, Ipv4Protocol2::Icmpv4),
-                    Icmpv4Header::new(Icmpv4Type2::EchoReply { id, seq_num }, 0),
+                        Ethernet2Header::new(dst_link_addr, rt.local_link_addr(), EtherType2::Ipv4),
+                        Ipv4Header::new(rt.local_ipv4_addr(), dst_ipv4_addr, Ipv4Protocol2::Icmpv4),
+                        Icmpv4Header::new(Icmpv4Type2::EchoReply { id, seq_num }, 0),
                 ));
             };
             if let Err(e) = r {
@@ -141,12 +145,13 @@ impl<RT: Runtime> Icmpv4Peer<RT> {
     /// Parses and handles a ICMP message.
     pub fn receive(&mut self, ipv4_header: &Ipv4Header, buf: RT::Buf) -> Result<(), Fail> {
         let (icmpv4_hdr, _) = Icmpv4Header::parse(buf)?;
+        debug!("ICMPv4 received {:?}", icmpv4_hdr);
         match icmpv4_hdr.icmpv4_type {
             Icmpv4Type2::EchoRequest { id, seq_num } => {
                 self.tx
                     .unbounded_send((ipv4_header.src_addr, id, seq_num))
                     .unwrap();
-            }
+                }
             Icmpv4Type2::EchoReply { id, seq_num } => {
                 if let Some(tx) = self.requests.borrow_mut().remove(&(id, seq_num)) {
                     let _ = tx.send(());
