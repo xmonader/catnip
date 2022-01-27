3023a6b7b905e8efc599ed7dca13081cfe2b59b9
Pedro Henrique Penna <ppenna@microsoft.com>
2022-01-27 06:43:17 -0800

    [build] Enhancement: Drop Unused Features

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,12 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
-#![cfg_attr(feature = "strict", deny(warnings))]
-#![feature(allocator_api)]
-#![feature(alloc_layout_extra)]
-#![feature(const_panic, const_alloc_layout)]
-#![feature(const_mut_refs, const_type_name)]
-#![feature(generators, generator_trait)]
+#![cfg_attr(feature = "strict", deny(clippy:all))]
 #![feature(new_uninit)]
-#![feature(maybe_uninit_uninit_array, maybe_uninit_extra, maybe_uninit_ref)]
 #![feature(never_type)]
-#![feature(raw)]
 #![feature(try_blocks)]
-#![deny(clippy::all)]
-#![recursion_limit = "512"]
 #![feature(test)]
 #![feature(min_type_alias_impl_trait)]
+#![recursion_limit = "512"]
 
 #[macro_use]
 extern crate num_derive;
